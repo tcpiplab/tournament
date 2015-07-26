38ad773118603e4c137261d6051674e0dd821b5f
Luke Sheppard <lshep.usc@gmail.com>
2015-07-26 14:31:21 -0700

    Add a 'drop' to prevent connecting to an old db

--- tournament.sql
+++ tournament.sql
@@ -1,7 +1,25 @@
--- Table definitions for the tournament project.
---
+-- Luke Sheppard
+-- lshep.usc[(at)]gmail.com
+-- Project 2 of the Fullstack Nanodegree at Udacity.
+-- July 25, 2015
+
+-- This SQL file is for the final assignment for the Udacity Relational 
+-- Databases course as part of the Fullstack Nanodegree. It should accompany 
+-- the following three files:
+
+--   README.txt
+--   tournament.py
+--   tournament_test.py
+
+-- make sure we are not stumbling on a previous db of the same name 
+DROP DATABASE IF EXISTS tournament;
+
 CREATE database tournament; 
 
+-- make sure we are connecting to the right db 
+\c tournament
+
+-- Table definitions for the tournament project.
 CREATE TABLE playernames (
 id serial PRIMARY KEY,
 name text);
